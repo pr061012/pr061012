99b55230cf8cf0d39912a3cd8d0bdf9b0e592aee
Aracturat <ndozmorov@yandex.ru>
2012-10-22 00:52:07 +0700

    [Object] Add ObjectFactory Class

---
+++ model/World/ObjectFactory/ObjectFactory.cpp
@@ -0,0 +1,22 @@
+/*
+    Copyright (c) 2012, pr061012 Team.
+    See the LICENSE file for copying permission.
+*/
+
+#include "ObjectFactory.h"
+
+
+ObjectFactory::ObjectFactory()
+{
+
+}
+
+ObjectFactory::~ObjectFactory()
+{
+
+}
+
+ObjectFactory::createObject(int type, std::map<std::string, void *> params)
+{
+
+}
---
+++ model/World/ObjectFactory/ObjectFactory.h
@@ -0,0 +1,44 @@
+/*
+    Copyright (c) 2012, pr061012 Team.
+    See the LICENSE file for copying permission.
+*/
+
+#ifndef OBJECTFACTORY_H
+#define OBJECTFACTORY_H
+
+#include "../Object/Object.h"
+
+/**
+  * @class ObjectFactory
+  * @brief ObjectFactory Class
+  */
+
+class ObjectFactory
+{
+public:
+    //**************************************************************************
+    // CONSTRUCTOR/DESTRUCTOR.
+    //**************************************************************************
+
+
+    /**
+     * @brief Constructor.
+     */
+    ObjectFactory();
+
+    /**
+     * @brief Destructor.
+     */
+    ~ObjectFactory();
+
+	/**
+     * @brief Create object
+     * @return Object*
+	 * @param  type
+	 * @param  params
+	 */
+    Object* createObject(int type, std::map<std::string,void*> params);
+
+};
+
+#endif // OBJECTFACTORY_H
