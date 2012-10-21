0681f187afc70324225ba4b27a54fce8c82eaae4
Melkogotto <melkogotto@gmail.com>
2012-10-21 14:46:55 +0700

    [Creatures] Add initialization for health and age.

--- model/World/Object/Creatures/Humanoid/Humanoid.cpp
+++ model/World/Object/Creatures/Humanoid/Humanoid.cpp
@@ -3,12 +3,22 @@
     See the LICENSE file for copying permission.
 */
 
+#include "../../../../BasicTypes.h"
+#include "../Defines.h"
 #include "Humanoid.h"
 
 Humanoid::Humanoid(const DecisionMaker & dmaker) :
     Creature(dmaker)
 {
     this -> type = HUMANOID;
+
+    // Randomly initialize some values.
+    this -> max_health = rand() % (HUM_HEALTH_MAX - HUM_HEALTH_MIN) + HUM_HEALTH_MIN + 1;
+    this -> max_age    = rand() % (HUM_MAX_AGE - HUM_MIN_AGE) + HUM_MIN_AGE + 1;
+
+    // Initialize other values.
+    this -> health = this -> max_health;
+    this -> age    = this -> max_age;
 }
 
 Humanoid::~Humanoid()
--- model/World/Object/Creatures/NonHumanoid/NonHumanoid.cpp
+++ model/World/Object/Creatures/NonHumanoid/NonHumanoid.cpp
@@ -3,12 +3,22 @@
     See the LICENSE file for copying permission.
 */
 
+#include "../../../../BasicTypes.h"
+#include "../Defines.h"
 #include "NonHumanoid.h"
 
 NonHumanoid::NonHumanoid(const DecisionMaker & dmaker) :
     Creature(dmaker)
 {
     this -> type = NONHUMANOID;
+
+    // Randomly initialize some values.
+    this -> max_health = rand() % (NHUM_HEALTH_MAX - NHUM_HEALTH_MIN) + NHUM_HEALTH_MIN + 1;
+    this -> max_age    = rand() % (NHUM_MAX_AGE - NHUM_MIN_AGE) + NHUM_MIN_AGE + 1;
+
+    // Initialize other values.
+    this -> health = this -> max_health;
+    this -> age    = this -> max_age;
 }
 
 NonHumanoid::~NonHumanoid()
