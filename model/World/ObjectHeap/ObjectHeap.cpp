de1b0917fc5becb4066f069cdf8ee441dd57c6bb
Aracturat <ndozmorov@yandex.ru>
2012-10-22 01:05:29 +0700

    [Object] Add ObjectHeap class

---
+++ model/World/ObjectHeap/ObjectHeap.cpp
@@ -0,0 +1,41 @@
+/*
+    Copyright (c) 2012, pr061012 Team.
+    See the LICENSE file for copying permission.
+*/
+
+#include "ObjectHeap.h"
+
+
+
+ObjectHeap::ObjectHeap()
+{
+
+}
+
+ObjectHeap::~ObjectHeap()
+{
+
+}
+
+Object* ObjectHeap::next()
+{
+
+}
+
+Object* ObjectHeap::next(int type)
+{
+
+}
+
+bool ObjectHeap::push(Object* object)
+{
+
+}
+
+bool ObjectHeap::remove(Object* object)
+{
+
+}
+
+
+
---
+++ model/World/ObjectHeap/ObjectHeap.h
@@ -0,0 +1,67 @@
+/*
+    Copyright (c) 2012, pr061012 Team.
+    See the LICENSE file for copying permission.
+*/
+
+#ifndef OBJECTHEAP_H
+#define OBJECTHEAP_H
+
+#include "../Object/Object.h"
+
+
+/**
+  * @class ObjectHeap
+  * @brief ObjectHeap class
+  */
+
+class ObjectHeap
+{
+public:
+
+    //******************************************************************************
+    // CONSTRUCTOR/DESTRUCTOR.
+    //******************************************************************************
+
+    /**
+     * @brief Constructor.
+     */
+    ObjectHeap();
+
+    /**
+     * @brief Destructor.
+     */
+     ~ObjectHeap();
+
+	/**
+     * @brief Return next object in world.
+     * @return Object*
+	 */
+    Object* next();
+
+
+	/**
+     * @brief Work as next(), but return object with certain type.
+     * @return Object*
+	 * @param  type
+	 */
+    Object* next(int type);
+
+
+	/**
+     * @brief Push object to heap.
+     * @param  object
+     * @return
+	 */
+    bool push(Object* object);
+
+
+	/**
+     * @brief Remove object from heap.
+	 * @param  object
+     * @return
+	 */
+    bool remove(Object* object);
+
+};
+
+#endif // OBJECTHEAP_H
