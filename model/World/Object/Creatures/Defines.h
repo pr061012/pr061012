b77638621f4cd8f4171a666b7372b9fd58da1bd5
Melkogotto <melkogotto@gmail.com>
2012-10-21 14:39:06 +0700

    [Creatures] Add creature's defines.

---
+++ model/World/Object/Creatures/Defines.h
@@ -0,0 +1,43 @@
+/*
+    Copyright (c) 2012, pr061012 Team.
+    See the LICENSE file for copying permission.
+*/
+
+#ifndef DEFINES_H
+#define DEFINES_H
+
+//******************************************************************************
+// HUMANOID'S DEFINES.
+// Prefix: HUM.
+//******************************************************************************
+
+// Humanoids maximum health should lay in range [HUM_HEALTH_MIN,
+// HUM_HEALTH_MAX].
+
+/// Minimum possible value of humanoid's max_health.
+#define HUM_HEALTH_MIN      50
+
+/// Maximum possible value of humanoid's max_health.
+#define HUM_HEALTH_MAX      100
+
+/// Maximum possible value of humanoid's max_age.
+#define HUM_MAX_AGE         50
+
+//******************************************************************************
+// NONHUMANOID'S DEFINES.
+// Prefix: NHUM.
+//******************************************************************************
+
+// NonHumanoids maximum health should lay in range [NHUM_HEALTH_MIN,
+// NHUM_HEALTH_MAX].
+
+/// Minimum possible value of non-humanoid's max_health.
+#define NHUM_HEALTH_MIN     50
+
+/// Maximum possible value of non-humanoid's max_health.
+#define NHUM_HEALTH_MAX     100
+
+/// Maximum possible value of non-humanoid's max_age.
+#define NHUM_MAX_AGE        50
+
+#endif // DEFINES_H
