b685b889fd24a351f51d3b4e633df4dc59c6878a
Melkogotto <melkogotto@gmail.com>
2012-10-22 22:12:59 +0700

    [VisualState] Initial commit.

---
+++ model/Utilities/VisualState/VisualState.cpp
@@ -0,0 +1,20 @@
+/*
+ *  Copyright (c) 2012, pr061012 Team.
+ *  See the LICENSE file for copying permission.
+ */
+
+#include "VisualState.h"
+
+//******************************************************************************
+// CONSTRUCTOR/DESTRUCTOR.
+//******************************************************************************
+
+VisualState::VisualState()
+{
+
+}
+
+VisualState::~VisualState()
+{
+
+}
