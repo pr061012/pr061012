fcee52167ea637c69f1ebd44c893c8fa8ef93ca9
Aracturat <ndozmorov@yandex.ru>
2012-10-22 08:54:23 +0700

    [God] Add God class

---
+++ model/God/God.h
@@ -0,0 +1,32 @@
+/*
+    Copyright (c) 2012, pr061012 Team.
+    See the LICENSE file for copying permission.
+*/
+
+#ifndef GOD_H
+#define GOD_H
+
+
+#include "../World/World.h"
+
+
+/**
+  * @class God
+  * @brief God class
+  */
+
+class God
+{
+public:
+
+    //**************************************************************************
+    // CONSTRUCTOR/DESTRUCTOR.
+    //**************************************************************************
+
+    God();
+
+    ~God();
+
+};
+
+#endif // GOD_H
