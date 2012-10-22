bc2c9524cfc78673a56ef816a44cd1666dce3f90
Melkogotto <melkogotto@gmail.com>
2012-10-22 16:35:58 +0700

    [Resource] Translated comments.

--- model/World/Object/Resource/Resource.h
+++ model/World/Object/Resource/Resource.h
@@ -9,10 +9,9 @@
 #include "../Tool/Tool.h"
 
 /**
-  * @class Resource
-  * @brief Resourse class
-  */
-
+ * @class Resource
+ * @brief Resource class contains information about one mineral deposit.
+ */
 class Resource : virtual public Object
 {
 public:
@@ -20,131 +19,129 @@ public:
     // CONSTRUCTOR/DESTRUCTOR.
     //**************************************************************************
 
-	Resource ( );
+    // TODO: Maybe constructor should receive some args?
 
-    ~Resource ( );
+    /**
+     * @brief Constructor.
+     */
+    Resource();
 
-	/**
-	 * Добыча ресурса. Возвращает кол-во добытого ресурса. Должен вызывать tool.use()
-	 * @return int
-	 * @param  tool
-	 */
-    int gather(Tool tool);
+    /**
+     * @brief Destructor.
+     */
+    ~Resource();
 
-	/**
-	 * Мб регенерация?
-	 */
+    /**
+     * @brief  Resource gathering. Should be spawned only by Tool.
+     * @param  tool that spawned gathering
+     * @return amount of gathered resource
+     */
+    unsigned int gather(Tool tool);
+
+    /**
+     * @brief Make a resource's step in the world.
+     *        (Maybe in this method we can regenerate resource.)
+     */
     void step();
 
-
-    //******************************************************************************
+    //**************************************************************************
     // ACCESSORS.
-    //******************************************************************************
+    //**************************************************************************
 
-	/**
-	 * Set the value of progress
-	 * Отображает прогресс добычи ресурса, ибо у нас дерево не одним ударом топора
-	 * рубится (сколько ударов осталось).
-	 * @param new_var the new value of progress
-	 */
+    /**
+     * @brief Set the value of progress.
+     * @param new_var the new value of progress
+     */
     void setProgress(int new_var);
 
-	/**
-	 * Get the value of progress
-	 * Отображает прогресс добычи ресурса, ибо у нас дерево не одним ударом топора
-	 * рубится (сколько ударов осталось).
-	 * @return the value of progress
-	 */
+    /**
+     * @brief Get the value of progress.
+     * @return the value of progress
+     */
     int getProgress();
 
-	/**
-	 * Set the value of subtype
-	 * @param new_var the new value of subtype
-	 */
+    /**
+     * @brief Set the value of subtype.
+     * @param new_var the new value of subtype
+     */
     void setSubtype(ResourceType new_var);
 
-	/**
-	 * Get the value of subtype
-	 * @return the value of subtype
-	 */
+    /**
+     * @brief Get the value of subtype.
+     * @return the value of subtype
+     */
     ResourceType getSubtype();
 
-	/**
-	 * Set the value of regeneration_rate
-	 * Скорость регенерации ресурса (либо кол-во шагов на восстановление 1 ресурса,
-	 * либо кол-во шагов полного восстановления, либо ещё чё-нибудь.
-	 * @param new_var the new value of regeneration_rate
-	 */
+    /**
+     * @brief Set the value of regeneration_rate.
+     * @param new_var the new value of regeneration_rate
+     */
     void setRegenerationRate(int new_var);
 
-	/**
-	 * Get the value of regeneration_rate
-	 * Скорость регенерации ресурса (либо кол-во шагов на восстановление 1 ресурса,
-	 * либо кол-во шагов полного восстановления, либо ещё чё-нибудь.
-	 * @return the value of regeneration_rate
-	 */
+    /**
+     * @brief Get the value of regeneration_rate.
+     * @return the value of regeneration_rate
+     */
     int getRegenerationRate();
 
-	/**
-	 * Set the value of gathered
-	 * ИМХО должны быть ресурсы добываемые (деревья, шахты, кусты, горы) и добытые
-	 * (готовые брёвна, камни, руда, корзина с ягодой).
-	 * Затем и нужен этот флаг.
-	 * @param new_var the new value of gathered
-	 */
+    /**
+     * @brief Set the value of gathered.
+     * @param new_var the new value of gathered
+     */
     void setGathered(bool new_var);
 
-	/**
-	 * Get the value of gathered
-	 * ИМХО должны быть ресурсы добываемые (деревья, шахты, кусты, горы) и добытые
-	 * (готовые брёвна, камни, руда, корзина с ягодой).
-	 * Затем и нужен этот флаг.
-	 * @return the value of gathered
-	 */
+    /**
+     * @brief Get the value of gathered.
+     * @return the value of gathered
+     */
     bool getGathered();
 
-	/**
-	 * Set the value of difficulty
-	 * Кол-во ходов на добычу ресурса.
-	 * @param new_var the new value of difficulty
-	 */
+    /**
+     * @brief Set the value of difficulty.
+     * @param new_var the new value of difficulty
+     */
     void setDifficulty(int new_var);
 
-	/**
-	 * Get the value of difficulty
-	 * Кол-во ходов на добычу ресурса.
-	 * @return the value of difficulty
-	 */
+    /**
+     * @brief Get the value of difficulty.
+     * @return the value of difficulty
+     */
     int getDifficulty();
 
-	/**
-	 * Set the value of amount_per_gather
-	 * Кол-во получаемого ресурса при добыче.
-	 * @param new_var the new value of amount_per_gather
-	 */
+    /**
+     * @brief Set the value of amount_per_gather.
+     * @param new_var the new value of amount_per_gather
+     */
     void setAmountPerGather(int new_var);
 
-	/**
-	 * Get the value of amount_per_gather
-	 * Кол-во получаемого ресурса при добыче.
-	 * @return the value of amount_per_gather
-	 */
+    /**
+     * @brief Get the value of amount_per_gather.
+     * @return the value of amount_per_gather
+     */
     int getAmountPerGather();
 
 private:
-    /// Отображает прогресс добычи ресурса, ибо у нас дерево не одним ударом топора рубится (сколько ударов осталось).
+    /// @brief Resource gathering process (Wood can't be hacked by one hit.
+    ///        Humanoid needs several hits to do that.)
     int progress;
+
     /// Type of resource
     ResourceType subtype;
-    /// Скорость регенерации ресурса (либо кол-во шагов на восстановление 1 ресурса, либо кол-во шагов полного восстановления, либо ещё чё-нибудь.
-    int regeneration_rate;
-    /// ИМХО должны быть ресурсы добываемые (деревья, шахты, кусты, горы) и добытые (готовые брёвна, камни, руда, корзина с ягодой).
-    bool gathered;
-    /// Кол-во ходов на добычу ресурса.
-    int difficulty;
-    /// Кол-во получаемого ресурса при добыче.
-    int amount_per_gather;
 
+    /// @brief Resource regeneration rate. (Either amount of steps to restore
+    ///        1 resource unit or amount of steps to full regeneration or smth
+    ///        else).
+    int regeneration_rate;
+
+    /// @brief Resource maybe gathered (trees, mountains, mines) or not (stones,
+    ///        log, ore).
+    bool gathered;
+
+    /// Amount of steps to gather this resource.
+    int difficulty;
+
+    /// Amount of resources received on each gather step.
+    int amount_per_gather;
 };
 
 #endif // RESOURCE_H
