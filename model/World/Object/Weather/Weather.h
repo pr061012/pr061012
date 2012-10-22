fc19a7637c8ff4f24b0814e09604ffbf4c3e0075
Melkogotto <melkogotto@gmail.com>
2012-10-22 17:38:51 +0700

    [Weather] Beautify code.

--- model/World/Object/Weather/Weather.h
+++ model/World/Object/Weather/Weather.h
@@ -5,81 +5,88 @@
 
 #ifndef WEATHER_H
 #define WEATHER_H
+
 #include "../Object.h"
 #include "../../ObjectHeap/ObjectHeap.h"
 
 /**
-  * @class Weather
-  * @brief Weather class
-  */
-
+ * @class Weather
+ * @brief Weather class
+ */
 class Weather : virtual public Object
 {
 public:
-
-    //******************************************************************************
+    //**************************************************************************
     // CONSTRUCTOR/DESTRUCTOR.
-    //******************************************************************************
+    //**************************************************************************
 
+    /**
+     * @brief Constructor.
+     */
     Weather();
 
+    /**
+     * @brief Destructor.
+     */
     ~Weather();
 
+    //**************************************************************************
+    // STEP.
+    //**************************************************************************
+
 	/**
-	 * Что-то делает с объектами в window.
+     * @brief Does something with objects in window.
 	 */
     void step();
 
-	/**
-	 * Set the value of subtype
-	 * @param new_var the new value of subtype
-	 */
-
-    //******************************************************************************
+    //**************************************************************************
     // ACCESSORS.
-    //******************************************************************************
+    //**************************************************************************
 
+	/**
+     * @brief Set the value of subtype.
+     * @param new_var   the new value of subtype
+     */
     void setSubtype(WeatherType new_var);
 
 	/**
-	 * Get the value of subtype
+     * @brief  Get the value of subtype.
 	 * @return the value of subtype
 	 */
     WeatherType getSubtype();
+
 	/**
-	 * Set the value of window
-	 * Указатель на окно в индексаторе. Задаётся при инициализации.
-	 * @param new_var the new value of window
+     * @brief Set the value of window.
+     * @param new_var   the new value of window
 	 */
     void setWindow(ObjectHeap * new_var);
 
 	/**
-	 * Get the value of window
-	 * Указатель на окно в индексаторе. Задаётся при инициализации.
+     * @brief  Get the value of window.
 	 * @return the value of window
 	 */
     ObjectHeap* getWindow();
 
 	/**
-	 * Set the value of view_area
-	 * Область воздействия. Нужен для Indexator для определения содержимого window.
-	 * @param new_var the new value of view_area
+     * @brief Set the value of view_area.
+     * @param new_var   the new value of view_area
 	 */
     void setViewArea(Frame new_var);
 
 	/**
-	 * Get the value of view_area
-	 * Область воздействия. Нужен для Indexator для определения содержимого window.
+     * @brief  Get the value of view_area
 	 * @return the value of view_area
 	 */
     Frame getViewArea();
 
 private:
-
+    /// Weather type.
     WeatherType subtype;
-    /// Указатель на окно в индексаторе. Задаётся при инициализации.
+
+    /// Pointer to the window in Indexator. Initializes in constructor.
     ObjectHeap * window;
-    /// Область воздействия. Нужен для Indexator для определения содержимого window.
+
+    /// Effect area. Needed for Indexator for window's contents finding.
     Frame view_area;
 };
 
