732934ed77b01e60cb1941949f5f3f79a50c02a5
Aracturat <ndozmorov@yandex.ru>
2012-10-24 02:31:06 +0700

    [Point] Add some comment

--- test/Point/main.cpp
+++ test/Point/main.cpp
@@ -1,3 +1,8 @@
+/*
+    Copyright (c) 2012, pr061012 Team.
+    See the LICENSE file for copying permission.
+*/
+
 #include <iostream>
 #include "../../model/Utilities/Point/Point.h"
 #include <assert.h>
@@ -6,21 +11,40 @@ using namespace std;
 
 int main()
 {
+    //**************************************************************************
+    // TEST CONSTRUCTOR/DESTRUCTOR.
+    //**************************************************************************
+
     Point p1(2.5, 3);
     Point p2(0, 4);
+
     assert(p1.getX() == 2.5 && p1.getY() == 3);
     assert(p2.getX() == 0   && p2.getY() == 4);
+
+    //**************************************************************************
+    // TEST OPERATORS.
+    //**************************************************************************
     assert(p1 + p2 == Point(2.5, 7));
     assert(p1 - p2 == Point(2.5, -1));
     assert(p1 != p2);
+
+    //**************************************************************************
+    // TEST DISTANCE.
+    //**************************************************************************
+
     p1.setX(4);
     p2.setY(0);
+
     assert(p1.getX() == 4 && p1.getY() == 3);
     assert(p2.getX() == 0   && p2.getY() == 0);
+
     assert(p1.getDistance(p2) == 5);
+
     p1 += Point(10, 5) -= Point(1, 13);
+
     assert(p1 == Point(13, -5));
     assert(-p1 == Point(-13, 5));
+
     cout << "Hello World!" << endl;
     return 0;
 }
