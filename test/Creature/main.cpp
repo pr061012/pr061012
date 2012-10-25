9bc1849a2f3f29c1801bef2a410f6e77b9210a9c
Melkogotto <melkogotto@gmail.com>
2012-10-25 23:18:34 +0700

    [Humanoid] Test draft.

---
+++ test/Creature/main.cpp
@@ -0,0 +1,15 @@
+#include "../../model/World/Object/Creatures/Creature.h"
+
+class CreatureTest : public Creature
+{
+    void step()
+    {
+
+    }
+};
+
+int main()
+{
+    CreatureTest test;
+    return 0;
+}
