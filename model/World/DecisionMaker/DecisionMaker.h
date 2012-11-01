b5519ef9a1126001ba6e31a3ab4297f056c90a79
Dasha123321 <dashannmm@gmail.com>
2012-11-01 20:03:58 +0700

    [DecisionMaker]: correction of code

--- model/World/DecisionMaker/DecisionMaker.h
+++ model/World/DecisionMaker/DecisionMaker.h
@@ -5,17 +5,22 @@
 
 #ifndef DECISION_MAKER_H
 #define DECISION_MAKER_H
+
 #define ATR_CONST_HUM 9
 #define ACT_CONST_HUM 9
 #define ATR_CONST_NON_HUM 5
 #define ACT_CONST_NON_HUM 5
+#define PATH_TO_HUM_MATRIX "/../../../res/humanoid_matrix_decision.txt"
+#define PATH_TO_NON_HUM_MATRIX "/../../../res/non_humanoid_matrix_decision.txt"
+
 // Temporary workaround.
 
 #include <armadillo>
 #include<fstream>
-#include"BasicTypes.h"
+#include "../../BasicTypes.h"
 #include<vector>
-#include<random>
+#include <stdlib.h>
+
 
 using namespace std;
 using namespace arma;
