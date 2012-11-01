1958501b2c5191d4b4dc27dd82329c9b0c3cd26e
frostuser <redthatmadcat@gmail.com>
2012-11-02 01:47:29 +0700

    [World] Added tiny test files for IWorld

---
+++ test/World/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "../../model/World/IWorld.h"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	cout << "Hello! As you probably noticed, I am just checking buildability of IWorld!";
+	cin.get();
+
+	return 0;
+}
