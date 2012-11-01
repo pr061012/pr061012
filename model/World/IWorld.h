6ecc22d0f040abc06e16a0fe1bca36092d9f858c
frostuser <redthatmadcat@gmail.com>
2012-11-02 01:16:40 +0700

    [World] Added few virtual methods to IWorld. Do I have to add same comments again?

--- model/World/IWorld.h
+++ model/World/IWorld.h
@@ -1,10 +1,31 @@
 #ifndef IWORLD_H
 #define IWORLD_H
 
+#include "../BasicTypes.h"
+#include "../../view/ViewObject.h"
+#include "PendingAction/PendingAction.h"
+#include "../God/God.h"
+
 class IWorld
 {
+public:
 
-}
+    //******************************************************************************
+    // WORLD <-> GOD METHODS.
+    //******************************************************************************
+
+    // TODO: Ask my fellow team members if we are going to
+    //       have multiple gods support.
+    virtual void addGodAction(/*IGod* god, */PendingAction* action) = 0;
+
+    //******************************************************************************
+    // WORLD <-> VIEW METHODS.
+    //******************************************************************************
+
+    virtual ViewObject** getViewObjectsInRange(double x, double y, double radius) = 0;
+
+    virtual WeatherType getWeatherAtPoint(double x, double y) = 0;
+};
 
 #endif // IWORLD_H
 
