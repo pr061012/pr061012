bcca7d90250f80b515cd98834d603b01a4cec7bd
K. Mingulov <melkogotto@gmail.com>
2012-11-03 12:33:13 +0700

    [ParamArray] Add param array.

---
+++ model/World/ParamArray/ParamArray.cpp
@@ -0,0 +1,41 @@
+/*
+    Copyright (c) 2012, pr061012 Team.
+    See the LICENSE file for copying permission.
+*/
+
+#include "ParamArray.h"
+
+//**************************************************************************
+// KEYS ACCESSING.
+//**************************************************************************
+
+void ParamArray::addKey(std::string key, int value)
+{
+    this -> map[key] = value;
+}
+
+int ParamArray::getValue(std::string key)
+{
+    std::map <std::string, int> :: iterator iter = this -> map.find(key);
+
+    if(iter == map.end())
+    {
+        // TODO: Throw an exception.
+    }
+
+    return iter -> second;
+}
+
+bool ParamArray::removeKey(std::string key)
+{
+    std::map <std::string, int> :: iterator iter = this -> map.find(key);
+
+    if(iter == map.end())
+    {
+        return false;
+    }
+
+    this -> map.erase(iter);
+
+    return true;
+}
