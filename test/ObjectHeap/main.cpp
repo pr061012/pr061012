6f9b7ca6f0ea86db44c4a5c2cc3f19563021a422
Aracturat <ndozmorov@yandex.ru>
2012-11-03 16:18:29 +0700

    [ObjHeap] Add some tests

--- test/ObjectHeap/main.cpp
+++ test/ObjectHeap/main.cpp
@@ -4,9 +4,43 @@
 */
 
 #include <assert.h>
+#include <iostream>
 
 #include "../../model/World/ObjectHeap/ObjectHeap.h"
 
+class AnyObject : public Object
+{
+private:
+    static int count;
+    int id;
+
+public:
+    AnyObject():Object(RESOURCE)
+    {
+        id = count;
+        count++;
+    }
+
+    ~AnyObject();
+    void print()
+    {
+        std::cout << id << std::endl;
+    }
+
+    std::vector <Action *> getActions()
+    {
+        std::vector<Action*> ret;
+        return ret;
+    }
+
+    void receiveMessage(Action * action)
+    {
+
+    }
+
+};
+
+int AnyObject::count=0;
 
 int main()
 {
@@ -16,6 +50,15 @@ int main()
 
     ///Create ObjectHeap
     ObjectHeap* heap = new ObjectHeap();
+    ObjectHeap::iterator iter;
 
-
+    for (int i = 0; i<5; i++)
+    {
+        AnyObject* var = new AnyObject();
+        heap -> push(dynamic_cast<Object*>(var));
+    }
+    for (iter = heap -> begin(RESOURCE); iter != heap -> end(RESOURCE); iter++)
+    {
+        (dynamic_cast<AnyObject*>(*iter))->print();
+    }
 }
