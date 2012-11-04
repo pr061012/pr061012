09c46672f8e326993fc253e8971522b2d3017f62
Dasha123321 <dashannmm@gmail.com>
2012-11-04 15:34:37 +0700

    [BasicFunc]: added body of inline func

--- model/BasicFunc.h
+++ model/BasicFunc.h
@@ -9,7 +9,9 @@
 /**
  * @brief The generator of random value
  */
-static inline int randFromRange(int low_boundary, int top_boundary);
-
+static inline int randFromRange(int low_boundary, int top_boundary)
+{
+    return rand() % (top_boundary - low_boundary) + low_boundary + 1;
+}
 
 #endif // BASIC_FUNC_H
