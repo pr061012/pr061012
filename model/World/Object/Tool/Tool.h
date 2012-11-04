bccab0eff061e16e86a8ab86b0b3a74bc1728c49
K. Mingulov <melkogotto@gmail.com>
2012-11-04 16:27:37 +0700

    [Tool] Complete Tool draft.

--- model/World/Object/Tool/Tool.h
+++ model/World/Object/Tool/Tool.h
@@ -22,7 +22,7 @@ public:
     /**
      * @brief Constructor.
      */
-    Tool(ToolType type);
+    Tool(ToolType type, ResourceType material);
 
     /**
      * @brief Destructor.
@@ -30,47 +30,58 @@ public:
     ~Tool();
 
     //**************************************************************************
-    // USAGE.
+    // TOOL ACTIONS.
     //**************************************************************************
 
-	/**
-     * @brief Use tool (decrease strength).
-	 */
-    void use();
+    /**
+     * @brief  Gets actions.
+     * @return vector with actions.
+     */
+    std::vector <Action> * getActions();
+
+    //**************************************************************************
+    // STRENGTH.
+    //**************************************************************************
+
+    /**
+     * @brief  Gets current tool's strength.
+     * @return the value of current strength
+     */
+    unsigned int getStrength();
+
+    /**
+     * @brief  Gets maximum possible tool's strength.
+     * @return the value of maximum strength
+     */
+    unsigned int getMaxStrength();
+
+    /**
+     * @brief Decreases current tool's strength.
+     * @param delta the value to subtract
+     */
+    void decreaseStrength(unsigned int delta);
+
+    /**
+     * @brief Increases current tool's strength.
+     * @param delta the value to add
+     */
+    void increaseStrength(unsigned int delta);
 
     //**************************************************************************
     // TYPE ACCESSORS.
     //**************************************************************************
 
 	/**
-	 * Get the value of subtype
-	 * @return the value of subtype
+     * @brief  Gets tool's type.
+     * @return tool's type
 	 */
     ToolType getSubtype();
 
-	/**
-	 * Get the value of material
-	 * @return the value of material
+    /**
+     * @brief  Gets tool's material.
+     * @return tool's material
 	 */
     ResourceType getMaterial();
-
-    //**************************************************************************
-    // ACCESSORS.
-    //**************************************************************************
-
-	/**
-	 * Set the value of quality
-	 * Качество изделия.
-	 * @param new_var the new value of quality
-	 */
-    void setQuality(int new_var);
-
-	/**
-	 * Get the value of quality
-	 * Качество изделия.
-	 * @return the value of quality
-	 */
-    int getQuality();
 
 private:
     /// Tool type.
@@ -79,8 +90,10 @@ private:
     /// Material of which tool is made.
     const ResourceType material;
 
-    /// Tool quality.
-    int quality;
+    /// Tool's current strength.
+    unsigned int current_strength;
+    /// Tool's maximum possible strength.
+    unsigned int max_strength;
 };
 
 #endif // TOOL_H
