3d9e80e088679a730b26fcb94c40987a6938995f
Dasha123321 <dashannmm@gmail.com>
2012-11-04 13:31:36 +0700

    [Creatures]: Changed the constructor, add health-methods

--- model/World/Object/Creatures/Creature.cpp
+++ model/World/Object/Creatures/Creature.cpp
@@ -6,6 +6,16 @@
 #include "Creature.h"
 
 //******************************************************************************
+// STATIC FUNCTIONS.
+//******************************************************************************
+
+static inline int randFromRange(int low_boundary, int top_boundary)
+{
+    return rand() % (top_boundary - low_boundary) + low_boundary + 1;
+}
+
+
+//******************************************************************************
 // CONSTRUCTOR/DESTRUCTOR.
 //******************************************************************************
 
@@ -15,7 +25,19 @@ Creature::Creature(CreatureType type, const DecisionMaker & dmaker) :
     brains(dmaker),
     inventory(new ObjectHeap)
 {
+	// Randomly initialize some values.
+    max_hunger      = randFromRange(HUNGER_MIN,     HUNGER_MAX);
+    max_sleepiness  = randFromRange(SLEEPINESS_MIN, SLEEPINESS_MAX);
+	int health = randFromRange(NHUM_HEALTH_MIN, NHUM_HEALTH_MAX);
 
+    // Initialize other values.
+	hunger      = 100 - max_hunger;
+    sleepiness  = 100 - max_sleepiness;
+	safety		= 0; //we need in function to calculate it
+					 //different for HUM and NON_HUM?
+	// Initialize some inhereted things.
+	setMaxHealth(health);
+    setHealth(100 - health);
 }
 
 Creature::~Creature()
@@ -66,3 +88,55 @@ unsigned int Creature::getMaxAge()
 {
     return this -> max_age;
 }
+
+
+void Creature::setHealth(unsigned int health)
+{
+    this -> health = health;
+}
+
+unsigned int Creature::getHealth()
+{
+    return this -> health;
+}
+
+void Creature::setMaxHealth(unsigned int max_health)
+{
+    this -> max_health = max_health;
+}
+
+unsigned int Creature::getMaxHealth()
+{
+    return this -> max_health;
+}
+
+
+//******************************************************************************
+// OBJECT'S LIFE.
+//******************************************************************************
+
+void Object::decreaseHealth(unsigned int delta)
+{
+	if(this -> health > delta)
+	{
+		this -> health -= delta;
+	}
+	else
+	{
+		this -> health = 0;
+		destroy();
+	}
+}
+
+void Object::increaseHealth(unsigned int delta)
+{
+    if(this -> health + delta < this->max_health)
+    {
+        this -> health += delta;
+    }
+    else
+    {
+        this -> health = this -> max_health;
+    }
+}
+
