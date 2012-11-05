5af94448e9d0b67ccb4fc1236fb302e09d9944b3
K. Mingulov <melkogotto@gmail.com>
2012-11-05 16:23:02 +0700

    [ParamArray] Update test.

--- test/ParamArray/main.cpp
+++ test/ParamArray/main.cpp
@@ -2,23 +2,45 @@
 #include <cassert>
 
 #include "../../model/World/ParamArray/ParamArray.h"
+#include "../../model/World/ParamArray/EParamArrayBadKey.h"
 
 int main()
 {
     ParamArray pa;
+    bool exc_was = false;
 
-    assert(pa.getValue("key") == 0);
+    // Testing exception throwing for getValue().
+    try
+    {
+        assert(pa.getValue("key") == 0);
+    }
+    catch(EParamArrayBadKey & exc)
+    {
+        exc_was = true;
+    }
+    assert(exc_was == true);
 
+    // Testing adding and getting values.
     pa.addKey("key", 1);
     assert(pa.getValue("key") == 1);
-
     pa.addKey("another_key", 2);
     assert(pa.getValue("another_key") == 2);
 
+    // Testing key remove.
     assert(pa.removeKey("key") == true);
-    assert(pa.getValue("key") == 0);
-
     assert(pa.removeKey("key") == false);
+
+    // Testing what key doesn't exist anymore.
+    exc_was = false;
+    try
+    {
+        assert(pa.getValue("key") == 0);
+    }
+    catch(EParamArrayBadKey & exc)
+    {
+        exc_was = true;
+    }
+    assert(exc_was == true);
 
     return 0;
 }
