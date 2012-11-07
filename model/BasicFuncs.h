e8d78c47b7cbb326e2cf20ef9312e4ba5ad8e140
Dasha123321 <dashannmm@gmail.com>
2012-11-07 22:59:07 +0700

    [BasicFuncs] add doubleRand

--- model/BasicFuncs.h
+++ model/BasicFuncs.h
@@ -18,4 +18,12 @@ static inline int randFromRange(int low_boundary, int top_boundary)
     return rand() % (top_boundary - low_boundary) + low_boundary + 1;
 }
 
+/**
+ * @brief Returns random double number.
+ */
+static inline double doubleRand(int top_boundary)
+{
+    top_boundary *= 100;
+    return (rand() % top_boundary + 1) / 100.0;
+}
 #endif // BASIC_FUNC_H
