0566d621237eec43f15d4201c864767201ab76b2
Aracturat <ndozmorov@yandex.ru>
2012-11-12 13:10:18 +0700

    [KeyHandler] Add TODO

--- view/KeyHandler/KeyHandler.cpp
+++ view/KeyHandler/KeyHandler.cpp
@@ -1,51 +1,54 @@
 #include "KeyHandler.h"
 
+// TODO: Change name this define
 #define SPEED 1
+
 //******************************************************************************
 // CONSTRUCTOR/DESTRUCTOR.
 //******************************************************************************
 
-KeyHandler::KeyHandler(View* v)
+KeyHandler::KeyHandler(View* v):
+    view(v)
 {
-    view = v;
-}
 
+}
 
 KeyHandler::~KeyHandler()
 {
 
 }
 
-// TODO: add 'speed' constant or variable instead of 0.05.
 void KeyHandler::handleKeys()
 {
-        if(glfwGetKey(GLFW_KEY_RIGHT))
+        if (glfwGetKey(GLFW_KEY_RIGHT))
         {
-            if(view->getX()+SPEED < VIEW_SCREEN_WIDTH)
+            if (view -> getX() + SPEED < VIEW_SCREEN_WIDTH)
             {
-                view->setX(view->getX()+SPEED);
-            }
-        }
-        if(glfwGetKey(GLFW_KEY_LEFT))
-        {
-            if(view->getX()-SPEED > 0)
-            {
-                view->setX(view->getX()-SPEED);
-            }
-        }
-        if(glfwGetKey(GLFW_KEY_DOWN))
-        {
-            if(view->getY()-SPEED > 0)
-            {
-                view->setY(view->getY()-SPEED);
-            }
-        }
-        if(glfwGetKey(GLFW_KEY_UP))
-        {
-            if(view->getY()+SPEED < VIEW_SCREEN_HEIGHT)
-            {
-                view->setY(view->getY()+SPEED);
+                view -> setX(view -> getX() + SPEED);
             }
         }
 
+        if (glfwGetKey(GLFW_KEY_LEFT))
+        {
+            if (view -> getX() - SPEED > 0)
+            {
+                view -> setX(view -> getX()-SPEED);
+            }
+        }
+
+        if (glfwGetKey(GLFW_KEY_DOWN))
+        {
+            if (view -> getY() - SPEED > 0)
+            {
+                view -> setY(view -> getY() - SPEED);
+            }
+        }
+
+        if (glfwGetKey(GLFW_KEY_UP))
+        {
+            if (view -> getY() + SPEED < VIEW_SCREEN_HEIGHT)
+            {
+                view -> setY(view -> getY() + SPEED);
+            }
+        }
 }
