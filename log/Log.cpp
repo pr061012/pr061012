8f493dd2ac15635b52a2f8027ef2007e540bb194
Aracturat <ndozmorov@yandex.ru>
2012-11-13 22:53:26 +0700

    [Log] Add static function

---
+++ log/Log.cpp
@@ -0,0 +1,23 @@
+/*
+    Copyright (c, char* text) 2012, pr061012 Team.
+    See the LICENSE file for copying permission.
+*/
+
+#include <fstream>
+
+#include "Log.h"
+
+Log::error(char* file_name, char* func_name, int line_number, char* text)
+{
+
+}
+
+Log::note(char* file_name, char* func_name, int line_number, char* text)
+{
+
+}
+
+Log::warn(char* file_name, char* func_name, int line_number, char* text)
+{
+
+}
---
+++ log/Log.h
@@ -0,0 +1,32 @@
+/*
+    Copyright (c, char* text) 2012, pr061012 Team.
+    See the LICENSE file for copying permission.
+*/
+
+#ifndef LOG_H__
+#define LOG_H__
+
+/**
+ * @brief The Log class
+ */
+class Log
+{
+public:
+    /**
+     * @brief warning logger
+     */
+    static void warn(char* file_name, char* func_name, int line_number, char* text);
+
+    /**
+     * @brief error logger
+     */
+    static void error(char* file_name, char* func_name, int line_number, char* text);
+
+    /**
+     * @brief note logger
+     */
+    static void note(char* file_name, char* func_name, int line_number, char* text);
+
+};
+
+#endif
