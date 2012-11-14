9a08fc8f50186ed8c982d27b4d3ac14a26526e77
Aracturat <ndozmorov@yandex.ru>
2012-11-14 16:08:13 +0700

    [Log] Some shanges

--- log/LogDefines.h
+++ log/LogDefines.h
@@ -1,5 +1,5 @@
+#include "Log.h"
+
 #define note(text)  note(__FILE__, __PRETTY_FUNCTION__, __LINE__,text)
 #define warn(text)  warn(__FILE__, __PRETTY_FUNCTION__, __LINE__,text)
 #define error(text) error(__FILE__, __PRETTY_FUNCTION__, __LINE__,text)
-
-
