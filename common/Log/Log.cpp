30798f597cfeb63e27d5b7f1640a9b3b7cf6e310
K. Mingulov <melkogotto@gmail.com>
2012-11-15 19:29:43 +0700

    [Log] Improve log output.

--- common/Log/Log.cpp
+++ common/Log/Log.cpp
@@ -12,22 +12,25 @@ std::ofstream Log::log_file("LOG");
 void Log::error(std::string file_name, std::string func_name,
                 int line_number, std::string text)
 {
-    log_file << "[ERROR] " << file_name << ":" << line_number;
-    log_file << " : " << func_name << " "<< text << std::endl;
+    log_file << "[ERROR] " << file_name << ":" << line_number << std::endl <<
+                "        " << func_name << std::endl <<
+                "        " << text << std::endl;
 }
 
 void Log::note(std::string file_name, std::string func_name,
                int line_number, std::string text)
 {
-    log_file << "[NOTE]  " << file_name << ":" << line_number;
-    log_file << " : " << func_name << " "<< text << std::endl;
+    log_file << "[NOTE]  " << file_name << ":" << line_number << std::endl <<
+                "        " << func_name << std::endl <<
+                "        " << text << std::endl;
 }
 
 void Log::warn(std::string file_name, std::string func_name,
                int line_number, std::string text)
 {
-    log_file << "[WARN]  " << file_name << ":" << line_number;
-    log_file << " : " << func_name << " "<< text << std::endl;
+    log_file << "[WARN]  " << file_name << ":" << line_number << std::endl <<
+                "        " << func_name << std::endl <<
+                "        " << text << std::endl;
 }
 
 
