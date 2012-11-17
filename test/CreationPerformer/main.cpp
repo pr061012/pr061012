5d1518858c7e3d2333c1ff60a9156018b26e8ed1
Aracturat <ndozmorov@yandex.ru>
2012-11-18 01:00:17 +0700

    [CreatPerf] Add empty test dir

---
+++ test/CreationPerformer/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <ctime>
+#include <random>
+#include <cstdlib>
+
+#include "../../common/BasicDefines.h"
+#include "../../common/Log/Log.h"
+#include "../../common/Exceptions/EInvalidResPath.h"
+#include "../../control/Controller/Controller.h"
+#include "../../model/World/World.h"
+#include "../../view/View.h"
+
+/// Period for world updating.
+const int PERIOD = CLOCKS_PER_SEC / TM_TICKS_PER_SECOND;
+
+int main()
+{
+    // Error flag.
+    bool error = false;
+
+    // Initializing random.
+    srand(time(NULL));
+
+    // Running game.
+    try
+    {
+        // Creating World, View and Controller.
+        World world(rand(), SZ_WORLD_HSIDE);
+        Controller control(&world);
+        View view(world);
+
+        int t0 = clock();
+
+        do
+        {
+            if(clock() - t0 > PERIOD)
+            {
+                t0 += PERIOD;
+                control.step();
+                view.redraw();
+            }
+        } while(view.continues());
+    }
+    catch(EInvalidResPath& exc)
+    {
+        error = true;
+        Log::ERROR("Failed to load game resource '" + exc.getResPath() +
+                   "'. Terminating.");
+    }
+    catch(std::bad_alloc& exc)
+    {
+        error = true;
+        Log::ERROR("Failed to allocate memory. Terminating.");
+    }
+
+    if(error)
+    {
+        std::cout << "An error occurred during game execution. Look for LOG " <<
+                     "file for details." << std::endl;
+    }
+
+    return 0;
+}
