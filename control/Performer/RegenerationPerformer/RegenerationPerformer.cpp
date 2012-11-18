50a85c264add237f82e381ffc36303639b781d79
Aracturat <ndozmorov@yandex.ru>
2012-11-18 13:52:22 +0700

    [Perf] Add some conditions

--- control/Performer/RegenerationPerformer/RegenerationPerformer.cpp
+++ control/Performer/RegenerationPerformer/RegenerationPerformer.cpp
@@ -4,6 +4,9 @@
 */
 
 #include "RegenerationPerformer.h"
+#include "../../../common/Random/Random.h"
+#include "../../../common/BasicDefines.h"
+
 #include <vector>
 
 RegenerationPerformer::RegenerationPerformer(Indexator& indexator):
@@ -20,18 +23,24 @@ RegenerationPerformer::~RegenerationPerformer()
 void RegenerationPerformer::perform(Action& action)
 {
     Object * actor = action.getActor();
-    std::vector<Object*> participants = action.getParticipants();
-    uint delta;
     ObjectType type = actor -> getType();
+
+    std::vector<Object*> participants = action.getParticipants();
+
+    uint delta = Random::int_num(actor->getHealthPoints());
+
     uint object_index = action.getParam<uint>("object_index");
+    uint tool_index = action.getParam<uint>("tool_index");
 
     ObjectHeap env = indexator.getAreaContents(actor->getShape());
     ObjectHeap::const_iterator iter = env.end();
 
     if ((type != RESOURCE) || (type != CREATURE))
     {
-
+        action.markAsFailed();
+        return;
     }
+
     if (env.find(participants[object_index], false) == iter)
     {
         action.markAsFailed();
