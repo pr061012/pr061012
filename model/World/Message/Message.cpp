caaa4713adb576a7fddfb0b04a538fc24ea18472
K. Mingulov <melkogotto@gmail.com>
2012-11-18 13:16:47 +0700

    [Message] Initial commit.

---
+++ model/World/Message/Message.cpp
@@ -0,0 +1,30 @@
+/*
+    Copyright (c) 2012, pr061012 Team.
+    See the COPYING file for copying permission.
+*/
+
+#include "Message.h"
+
+//******************************************************************************
+// CONSTRUCTOR/DESTRUCTOR
+//******************************************************************************
+
+Message::Message(const MessageType type, const Object* sender) :
+    type(type),
+    sender(sender)
+{
+}
+
+//******************************************************************************
+// ACCESSING MESSAGE TYPE AND SENDER.
+//******************************************************************************
+
+MessageType Message::getType() const
+{
+    return this -> type;
+}
+
+const Object* Message::getSender() const
+{
+    return this -> sender;
+}
---
+++ model/World/Message/Message.h
@@ -0,0 +1,61 @@
+/*
+    Copyright (c) 2012, pr061012 Team.
+    See the COPYING file for copying permission.
+*/
+
+#ifndef MESSAGE_H
+#define MESSAGE_H
+
+#include <string>
+#include <vector>
+#include <map>
+
+#include "../Object/Object.h"
+#include "../../../common/BasicTypes.h"
+
+/**
+ * @brief Messages are mechanism for communication between objects.
+ */
+class Message
+{
+public:
+    //**************************************************************************
+    // CONSTRUCTOR/DESTRUCTOR
+    //**************************************************************************
+
+    /**
+     * @brief Constructor.
+     * @param type  message's type
+     */
+    Message(const MessageType type, const Object* sender);
+
+    /**
+     * @brief Destructor,
+     */
+    ~Message();
+
+    //**************************************************************************
+    // ACCESSING MESSAGE TYPE AND SENDER.
+    //**************************************************************************
+
+    /**
+     * @brief  Gets type of message.
+     * @return type of message
+     */
+    MessageType getType() const;
+
+    /**
+     * @brief  Gets message's sender.
+     * @return message's sender
+     */
+    const Object* getSender() const;
+
+private:
+    /// Message type.
+    const MessageType type;
+
+    /// Message's sender.
+    const Object* sender;
+};
+
+#endif // MESSAGE_H
