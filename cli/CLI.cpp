12766106bce606b8c60123a8a5e421226b539704
K. Mingulov <melkogotto@gmail.com>
2012-11-22 00:10:25 +0700

    [CLI] Add draft of runCommand() method.

--- cli/CLI.cpp
+++ cli/CLI.cpp
@@ -5,13 +5,72 @@
 
 #include "CLI.h"
 
+#include <sstream>
+
+//******************************************************************************
+// CONSTRUCTOR.
+//******************************************************************************
+
 CLI::CLI(World* world, Controller* control) :
     world(world),
     control(control)
 {
 }
 
+//******************************************************************************
+// RUNNING COMMANDS.
+//******************************************************************************
+
 std::string CLI::runCommand(std::string command)
 {
+    // Creating string stream.
+    std::stringstream ss(command);
+
+    // Reading command.
+    std::string cmd;
+    ss >> cmd;
+
+    // Reading command args and running processors.
+    std::string output;
+    if (cmd == "init")
+    {
+    }
+    else if (cmd == "random-init")
+    {
+    }
+    else if (cmd == "create")
+    {
+    }
+    else if (cmd == "list")
+    {
+    }
+    else if (cmd == "info")
+    {
+    }
+    else if (cmd == "change")
+    {
+    }
+    else if (cmd == "step")
+    {
+        output = this -> step();
+    }
+    else if (cmd == "trace-step")
+    {
+    }
+    else
+    {
+        output = "Unknown command `" + cmd + "`.\n";
+    }
+
+    return output;
+}
+
+//******************************************************************************
+// COMMAND'S PROCESSORS.
+//******************************************************************************
+
+std::string CLI::step()
+{
+    this -> control -> step();
     return std::string();
 }
