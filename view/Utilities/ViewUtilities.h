7b0899ddb86ae549141ee5f64e72e079b7a38a1b
frostuser <redthatmadcat@gmail.com>
2012-12-02 22:10:32 +0700

    [ViewUtilities] Simple tab symbols workaround.

--- view/Utilities/ViewUtilities.h
+++ view/Utilities/ViewUtilities.h
@@ -16,6 +16,11 @@ class ViewUtilities
 public:
     static void renderText(double x, double y, double size, std::string msg, double line_size = 0.0)
     {
+        for(int i = 0; i < msg.length(); ++i)
+        {
+            if(msg[i] == '\t')msg[i] = ' ';
+        }
+
         const char* txt = msg.c_str();
 
         int scr_width, scr_height;
