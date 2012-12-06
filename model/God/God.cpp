c76dc15b327229c29ffc729609f169333b12e1ec
Aracturat <ndozmorov@yandex.ru>
2012-12-06 14:14:59 +0700

    [God] Add setWeatherAt()

--- model/God/God.cpp
+++ model/God/God.cpp
@@ -4,12 +4,17 @@
 */
 
 #include "God.h"
+#include "../World/Action/Action.h"
+#include "../../common/BasicDefines.h"
+#include "../../common/BasicTypes.h"
+#include "../../common/Math/Random.h"
 
 //******************************************************************************
 // CONSTRUCTOR/DESTRUCTOR.
 //******************************************************************************
 
-God::God()
+God::God(World& world):
+    world(world)
 {
 
 }
@@ -17,4 +22,17 @@ God::God()
 God::~God()
 {
 
+}
+
+void God::setWeatherAt(Vector x, WeatherType type)
+{
+    actions.clear();
+
+    Action act(CREATE_OBJ, nullptr);
+    act.addParam<Vector>("coord", x);
+    act.addParam<WeatherType>("weat_type", type);
+    uint weat_steps = Random::double_num(WEAT_STEPS_MIN, WEAT_STEPS_MAX);
+    act.addParam<uint>("weat_steps", weat_steps);
+
+    actions.push_back(act);
 }
